f6a26cadacbced4fa82e112ab616ca2efac3c4a3
tarohoek <91067554+tarohoek@users.noreply.github.com>
2023-02-03 10:20:09 +0100

    Delete news.js
    yeet

--- Javascript/news.js
+++
@@ -1,8 +0,0 @@
-var url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=b4809d1f05334dfb8b91385ea5b422f9';
-
-var req = new Request(url);
-
-fetch(req)
-    .then(function(response) {
-        console.log(response.json());
-    })
