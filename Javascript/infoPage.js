be2258d1a52bb4cf2790c459ae2dae9ee32d7a57
PandaStew <thierry1201@hotmail.com>
2023-02-21 10:39:49 +0100

    cultuurbarbaar
    nhext category

--- Javascript/infoPage.js
+++ Javascript/infoPage.js
@@ -1,14 +1,20 @@
 $("#actueel").click(function(){
     $(".cat2").hide();
     $(".cat1").show();
-    console.log("hallo")
+    $(".cat3").hide();
 });
 $("#sport").click(function(){
     $(".cat1").hide();
     $(".cat2").show();
-    console.log("je moeder")
+    $(".cat3").hide();
 });
-$("#Alles").click(function(){
+$("#alles").click(function(){
     $(".cat1").show();
     $(".cat2").show();
+    $(".cat3").show();
+});
+$("#cultuur").click(function(){
+    $(".cat1").hide();
+    $(".cat2").hide();
+    $(".cat3").show();
 });
