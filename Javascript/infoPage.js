ef0932977e514c9b59580cbc391ba4e9c8f34b09
PandaStew <thierry1201@hotmail.com>
2023-02-21 09:54:26 +0100

    Update infoPage.js

--- Javascript/infoPage.js
+++ Javascript/infoPage.js
@@ -1,8 +1,10 @@
 $("#category1").click(function(){
     $(".cat2").hide();
     $(".cat1").show();
+    console.log("hallo")
 });
 $("#sport").click(function(){
     $(".cat1").hide();
     $(".cat2").show();
+    console.log("je moeder")
 });
