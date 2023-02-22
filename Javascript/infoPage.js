be7588d888b0b9d93c87620f14f9a21621063a7f
tarohoek <taro.hoek@student.nhlstenden.com>
2023-02-22 08:48:59 +0100

    Update infoPage.js
    removed console logs

--- Javascript/infoPage.js
+++ Javascript/infoPage.js
@@ -21,21 +21,17 @@ $("#kultuur").click(function(){
    
     var iframes = $('.cat2');
     let data = iframes.length;
-    console.log("iframes recent:" + data);
     document.getElementById('recent').innerHTML = data;
     
 
     var iframes = $('.all');
-    let secondData = iframes.length;
-    console.log("alle iframes:" + secondData) 
+    let secondData = iframes.length; 
     document.getElementById('alles').innerHTML= secondData;    
     
     var iframes = $('.cat3');
     let thirdData = iframes.length;
-    console.log("sport iframes:" + thirdData); 
     document.getElementById('sport').innerHTML= thirdData;    
     
     var iframes = $('.cat4');
     let finalData = iframes.length;
-    console.log("kultuur iframes:" + finalData); 
     document.getElementById('culture').innerHTML= finalData;   
